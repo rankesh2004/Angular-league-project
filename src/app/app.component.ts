4d0ea3f1f2eedb53c3e03459006e963c841f9d6f
rankesh2004 <rangesh.csr.2004@gmail.com>
2024-08-19 12:56:07 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LeagueComponent } from './league/league.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LeagueComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-app';
+  counter = 0;
+
+  clickAdd(){
+    this.counter++;
+  }
+
+  clickSub(){
+    this.counter--;
+  }
 }
---
+++ src/app/league/league.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-league',
+  standalone: true,
+  imports: [],
+  templateUrl: './league.component.html',
+  styleUrl: './league.component.css'
+})
+export class LeagueComponent {
+
+}
